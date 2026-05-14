ef86b881fa6e475706b5d5ec1109cd53f39c9b24
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-05-14 19:52:53 +0300

    DNode

--- MyList.h
+++ MyList.h
@@ -70,8 +70,50 @@ public:
 
 
 
+template <typename U>
+class DList {
+    DNode<U>* head;
+    DNode<U>* current;
+public:
+    DList() { head = nullptr; current = nullptr; }
 
+    void push_back(U value) { //Добавлення нового вузла в кінець списку
+        //1. Створюємо новий вузол
+        DNode<U>* item = new DNode<U>(value);
 
+        //2. Чи ПЕРШИЙ вузол в списку?
+        if (head == nullptr || current == nullptr) {
+            head = current = item;
+        }else {
+            //3. НЕ перший вузол
+            current->next = item;
+            item->prev = current;
+
+            current = item;
+        }
+
+    }
+    void show() {//Виведення списку на екран
+        DNode<U>* p =  head;
+        while (p!= nullptr) {
+            cout << p->info << " ";
+            p = p->next;
+        }
+        cout << endl;
+    }
+    void show_reverse() {
+        DNode<U>* p = current;
+        while (p!= nullptr) {
+            cout << p->info << " ";
+            p = p->prev;
+        }
+        cout << endl;
+    }
+
+    DNode<U>* search(U value) { //Пошук вузла із заданим значенням
+
+    }
+};
 
 
 
